0e97803d844b8e57abeb7551a321e88e9e1516a7
Sebastian <sebastiansloma@gmail.com>
2023-05-08 07:19:26 +0200

    .

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -5,7 +5,7 @@ import { RAPID_API_KEY} from '@env'
 
 const rapidApiKey = RAPID_API_KEY
 
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
@@ -18,9 +18,26 @@ const useFetch = (endpoint) => {
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
         },
         params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'
+         ...query
         },
       };
+    const fetchData = async () =>{
+        setIsLoading(true)
+
+        try{
+            const response = await axios.request(options)
+            
+            setData(response.data.data)
+            setIsLoading(false)
+        }catch(error){
+            setError(error)
+            alert('There is an error')
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
 };
