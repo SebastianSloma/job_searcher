dc4dbf4399783f044d4b4cbfd44f623f0c36cfd0
Sebastian <sebastiansloma@gmail.com>
2023-05-29 12:44:59 +0200

    import to [id].js

---
+++ app/job-details/[id].js
@@ -0,0 +1,11 @@
+import {Text, View, SafeAreaView,ScrollView, ActivityIndicator, RefreshControl} from 'react-native'
+import {Stack, useRouter, useSearchParams} from 'expo-router'
+import { useCallback, useState } from 'react'
+
+const JobDetails =()=>{
+    return(
+        <Text>JobDetails</Text>
+    )
+}
+
+export default JobDetails
