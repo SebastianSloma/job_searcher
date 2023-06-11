7df4884f5d94b1ae7f03d8e0e9951e85ba81d87f
Sebastian <sebastiansloma@gmail.com>
2023-06-11 12:27:54 +0200

    .

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -5,15 +5,29 @@ import styles from './tabs.style';
 
 import { SIZES } from '../../../constants';
 
+const TabButton = ({ name, activeTab, onHandleSearchType }) =>
+(
+  <TouchableOpacity>
+    <Text>{name}</Text>
+  </TouchableOpacity>
+);
+
 const Tabs = ({ tabs, activeTab, setActiveTab }) => {
 	return (
 		<View style={styles.container}>
 			<FlatList
-      data={tabs}
-      renderItem={({item}) =>(
-        <TabButton/>
-      )}
-      />
+				data={tabs}
+				renderItem={({ item }) => (
+					<TabButton
+						name={item}
+						activeTab={activeTab}
+						onHandleSearchType={() => setActiveTab(item)}
+					/>
+				)}
+        horizontal
+        showsHorizontalScrollIndicator
+        keyExtractor={item => item}
+			/>
 		</View>
 	);
 };
