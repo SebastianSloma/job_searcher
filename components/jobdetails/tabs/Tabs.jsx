54651e2f0aab8f2526b1f4e99999f79bdac3d1c8
Sebastian <sebastiansloma@gmail.com>
2023-06-11 12:18:25 +0200

    preapere tabs.jsx

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 
-import styles from './tabs.style'
+import styles from './tabs.style';
 
-const Tabs = () => {
-  return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
-  )
-}
+import { SIZES } from '../../../constants';
 
-export default Tabs
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+	return (
+		<View style={styles.container}>
+			<FlatList
+      data={tabs}
+      />
+		</View>
+	);
+};
+
+export default Tabs;
