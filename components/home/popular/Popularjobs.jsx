d9f84b59cf77498c47659cea482e2afac738b2bb
Sebastian <sebastiansloma@gmail.com>
2023-06-25 13:08:53 +0200

    add heandlepresscard to popjob

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -23,7 +23,12 @@ const Popularjobs = () => {
 		num_pages: 1
 	})
 
-	console.log(data);
+	const [selectedJob, setSelectedJob] = useState()
+
+	const handleCardPress = (item) =>{
+		router.push(`/job-details/${item.job_id}`)
+		setSelectedJob(item.job_id)
+	}
 
 	return (
 		<View style={styles.container}>
